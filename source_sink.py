75ad8bfbe21ffef91623f0f1595821058c6e6604
marshingjay <jam605@gmail.com>
2020-02-17 09:11:19 -0600

    made var names make more sense, cleaned up code

--- source_sink.py
+++ source_sink.py
@@ -4,8 +4,8 @@ import sys
 if sys.argv[1] != 'taint' and sys.argv[1] != 'sts':
     print('please use a correct-command line argument, try\npython3 source_sink.py sts\nor\npython3 source_sink.py taint\n')
     exit(42069)
-
-sources = set(["Node0x7f62940fa5c0", "Node0x7f6294096c10", "Node0x7f6294275a60"])
+sources = set(["Node0x7f629345e2f0"])
+# sources = set(["Node0x7f62940fa5c0", "Node0x7f6294096c10", "Node0x7f6294275a60", "Node0x7f62937f65e0", "Node0x7f629345e2f0", "Node0x7f6293bf4160", "Node0x7f62940c07f0"])
 sinks = set(["Node0x7f62940e02b0"])
 
 destination_regex = re.compile("^.*? -> (.*)\[style.*$")
@@ -18,49 +18,56 @@ f.seek(0)
 
 class Source:
     def __init__(self):
-        self.visited_edges = []
-        self.output_edges = []
+        self.visited_nodes = []
+        self.output_nodes = []
+        self.source_node = 'fake_source_node_id' 
+        self.sink_node = 'fake_sink_node_id'
 
-def GSTSEP(cur_edge, my_source):
-    if cur_edge in my_source.visited_edges:
-        my_source.output_edges.remove(cur_edge)
+def GSTSEP(cur_node, my_source):
+    if cur_node in my_source.visited_nodes:
+        my_source.output_nodes.remove(cur_node)
         return False
     else:
-        my_source.visited_edges += [cur_edge]
-    print(cur_edge)
-    if is_sink(cur_edge):
+        my_source.visited_nodes += [cur_node]
+    if is_sink(cur_node):
+        my_source.sink_node = cur_node
         return True
     else:
-        for x in getConnectingEdges(cur_edge):
-            if x not in my_source.output_edges:
-                my_source.output_edges += [x]
+        for x in get_connecting_nodes(cur_node):
+            if x not in my_source.output_nodes:
+                my_source.output_nodes += [x]
                 if GSTSEP(x, my_source):
                     return True
-        if cur_edge == "Node_Start":
+        if cur_node == "Node_Start":
             print("Path from source to sink not found")
-            exit()
-        my_source.output_edges.remove(cur_edge)
+            return False
+        my_source.output_nodes.remove(cur_node)
         return False
 
-def show_taint(cur_edge, my_source):
-    for x in getConnectingEdges(cur_edge):
-        if x not in my_source.output_edges:
-            my_source.output_edges += [x]
+def find_missing_fusion(nodes):
+    #what
+    print('test')
+
+
+def show_taint(cur_node, my_source):
+    for x in get_connecting_nodes(cur_node):
+        if x not in my_source.output_nodes:
+            my_source.output_nodes += [x]
             show_taint(x, my_source)
 
-def is_sink(edge):
-    return edge in sinks
+def is_sink(node):
+    return node in sinks
 
-def isLeaf(edge):
-    return not (edge in dot_dict)
+def is_leaf(node):
+    return not (node in dot_dict)
 
-def getConnectingEdges(edge):
-    if edge in dot_dict:
-        return dot_dict[edge]
+def get_connecting_nodes(node):
+    if node in dot_dict:
+        return dot_dict[node]
     else:
         return []
         
-def output_final_dot_graph(edges):
+def output_final_dot_graph(nodes):
     output_file = open('output.dot', 'w')
     output_file.write('digraph "SVFG" {\n')
     output_file.write('\tlabel="SVFG";\n')
@@ -69,7 +76,7 @@ def output_final_dot_graph(edges):
         node_def = definition_regex.match(x)
         node_src = source_regex.match(x)
         node_dest = destination_regex.match(x)
-        if (node_def != None and node_def.group(1) in edges) or ((node_src != None and node_src.group(1) in edges) and (node_dest != node_dest.group(1) in edges)):
+        if (node_def != None and node_def.group(1) in nodes) or ((node_src != None and node_src.group(1) in nodes) and (node_dest != node_dest.group(1) in nodes)):
             output_file.write(x + '\n')
     output_file.write('}\n')
 
@@ -94,11 +101,13 @@ for line in f:
 
 final_nodes = set()
 for x in sources:
+    print(x)
     my_new_source = Source()
+    my_new_source.source_node = x
     dot_dict["Node_Start"] = [x]
     if sys.argv[1] == 'taint':
         show_taint("Node_Start", my_new_source)
     elif sys.argv[1] == 'sts':
         GSTSEP("Node_Start", my_new_source)
-    final_nodes = final_nodes.union(my_new_source.output_edges)
+    final_nodes = final_nodes.union(my_new_source.output_nodes)
 output_final_dot_graph(final_nodes)
